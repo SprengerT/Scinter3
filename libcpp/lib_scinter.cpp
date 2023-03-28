4eb0111e2e368484a9f30cba70e82b37db60c5b2
Tim Sprenger <tsprenger@mpifr-bonn.mpg.de>
2023-03-28 15:12:45 +0200

    synchronization to local working branch

--- libcpp/lib_scinter.cpp
+++ libcpp/lib_scinter.cpp
@@ -241,7 +241,8 @@ void  SumStatPoints_2scr (double *E_real, double *E_im, int N_t, int N_nu, doubl
 
 extern "C"
 void  SumStatPoints_2scr_CP (double *E_real, double *E_im, int N_t, int N_nu, double *t, double *nu, int N_x, int N_y, double *im_x, double *im_y, double D_x, double D_y, double D_s,
-	double V_x, double V_y, double *V_p_ra, double *V_p_dec, double V_s_ra, double V_s_dec, double a_x, double a_y, double *mu_x, double *mu_y, double *phi_x, double *phi_y, double mu_CPx, double mu_CPy)
+	double V_x, double V_y, double *V_p_ra, double *V_p_dec, double V_s_ra, double V_s_dec, double a_x, double a_y, double *mu_x, double *mu_y, double *phi_x, double *phi_y, double mu_CPx, double mu_CPy,
+    double p_ra, double p_dec, double s_ra, double s_dec )
 {
 	#define  REAL(i_t,i_nu)  E_real[(i_t)*N_nu + (i_nu)]
 	#define  IMAG(i_t,i_nu)  E_im[(i_t)*N_nu + (i_nu)]
@@ -282,10 +283,10 @@ void  SumStatPoints_2scr_CP (double *E_real, double *E_im, int N_t, int N_nu, do
 		double fy_p_ort = -s*D_ys/D_s;
 		double fy_s_ort = -s*c*D_x*D_ys/(D_s*D_xs);
 		
-		double p_par = V_p_par*t[i_t];
-		double p_ort = V_p_ort*t[i_t];
-		double s_par = V_s_par*t[i_t];
-		double s_ort = V_s_ort*t[i_t];
+		double p_par = V_p_par*t[i_t] + p_ra*cos(a_x) + p_dec*sin(a_x);
+		double p_ort = V_p_ort*t[i_t] - p_ra*sin(a_x) + p_dec*cos(a_x);
+		double s_par = V_s_par*t[i_t] + s_ra*cos(a_x) + s_dec*sin(a_x);
+		double s_ort = V_s_ort*t[i_t] - s_ra*sin(a_x) + s_dec*cos(a_x);
 		double x;
 		double y;
 		
@@ -328,6 +329,159 @@ void  SumStatPoints_2scr_CP (double *E_real, double *E_im, int N_t, int N_nu, do
 			Phase = nu[i_nu]*Phi_opt;
 			REAL(i_t,i_nu) += mu_CPx*mu_CPy*cos(Phase);
 			IMAG(i_t,i_nu) += mu_CPx*mu_CPy*sin(Phase);
+		}
+	}
+	
+	#undef  REAL
+	#undef  IMAG
+}
+
+extern "C"
+void  SumStatPoints_1scr_atTel_CP (double *E_real, double *E_im, int N_t, int N_nu, double *t, double *nu, int N_x, double *im_x, double D_x, double D_s, double V_x, 
+	double *V_p_ra, double *V_p_dec, double *p_ra, double *p_dec, double V_s_ra, double V_s_dec, double a_x, double *mu_x, double *phi_x, double mu_CPx)
+{
+	#define  REAL(i_t,i_nu)  E_real[(i_t)*N_nu + (i_nu)]
+	#define  IMAG(i_t,i_nu)  E_im[(i_t)*N_nu + (i_nu)]
+	
+	#pragma omp parallel for
+	for (int i_t = 0; i_t < N_t; i_t++){
+		double Phase;
+		double Phi_opt;
+		double vc = 299792458.;
+		double D_xs = D_s-D_x;
+		double V_p_par = V_p_ra[i_t]*cos(a_x) + V_p_dec[i_t]*sin(a_x);
+		//double V_p_ort = -V_p_ra[i_t]*sin(a_x) + V_p_dec[i_t]*cos(a_x);
+		double V_s_par = V_s_ra*cos(a_x) + V_s_dec*sin(a_x);
+		//double V_s_ort = -V_s_ra*sin(a_x) + V_s_dec*cos(a_x);
+		double p_par = p_ra[i_t]*cos(a_x) + p_dec[i_t]*sin(a_x);
+		//double tel_ort = -p_ra[i_t]*sin(a_x) + p_dec[i_t]*cos(a_x);
+		
+		double Deff = D_x*D_s/D_xs;
+		double Veff_par = V_p_par + D_x/D_xs*V_s_par - D_s/D_xs*V_x;
+		//cout << Deff << "\t" << Veff_par << "\n";
+		
+		//double p_par = tel_par + V_p_par*t[i_t];
+		//double p_ort = tel_ort + V_p_ort*t[i_t];
+		//double s_par = V_s_par*t[i_t];
+		//double s_ort = V_s_ort*t[i_t];
+		
+		double f_delay = M_PI/vc*Deff/D_x/D_x;
+		double f_doppler = -2.*M_PI/vc*(p_par + Veff_par*t[i_t])/D_x;
+		
+		for (int i_x = 0; i_x < N_x; i_x++){
+			Phi_opt = f_delay*im_x[i_x]*im_x[i_x] + f_doppler*im_x[i_x];
+			for (int i_nu = 0; i_nu < N_nu; i_nu++){
+				Phase = phi_x[i_x] + nu[i_nu]*Phi_opt;
+				REAL(i_t,i_nu) += mu_x[i_x]*cos(Phase);
+				IMAG(i_t,i_nu) += mu_x[i_x]*sin(Phase);
+			}
+		}
+	}
+	
+	#undef  REAL
+	#undef  IMAG
+}
+
+extern "C"
+void  SumStatPoints_1scr_PS_CP (double *E_real, double *E_im, int N_t, int N_nu, double *t, double *nu, int N_x, double *im_x, double D_x, double D_s, double V_x, 
+	double *V_p_ra, double *V_p_dec, double *p_ra, double *p_dec, double V_s_ra, double V_s_dec, double s_par, double a_x, double *mu_x, double *phi_x, double mu_CPx)
+{
+	#define  REAL(i_t,i_nu)  E_real[(i_t)*N_nu + (i_nu)]
+	#define  IMAG(i_t,i_nu)  E_im[(i_t)*N_nu + (i_nu)]
+	
+	#pragma omp parallel for
+	for (int i_nu = 0; i_nu < N_nu; i_nu++){
+		double Phase;
+		double vc = 299792458.;
+		double D_xs = D_s-D_x;
+		double V_p_par;
+		double V_s_par = V_s_ra*cos(a_x) + V_s_dec*sin(a_x);
+		double p_par;
+        //double s_par = s_ra*cos(a_x) + s_dec*sin(a_x);
+		
+		//double Deff = D_x*D_s/D_xs;
+		//double Veff_par;
+		double Viss;
+		
+		//double f_delay = M_PI/vc*Deff/D_x/D_x;
+		//double f_doppler = -2.*M_PI/vc*( (p_par + s_par/D_xs*D_x + Veff_par*t[i_t])/D_x );
+        
+        double f = M_PI/vc*(1./D_x+1./D_s)*nu[i_nu];
+		double c_vel = D_x/D_xs*V_s_par - D_s/D_xs*V_x;
+		
+        double shift = 0.;
+		double dt = t[1]-t[0];
+		double pos;
+		
+		for (int i_t = 0; i_t < N_t; i_t++){
+			V_p_par = V_p_ra[i_t]*cos(a_x) + V_p_dec[i_t]*sin(a_x);
+			p_par = p_ra[i_t]*cos(a_x) + p_dec[i_t]*sin(a_x);
+			//Veff_par = V_p_par + D_x/D_xs*V_s_par - D_s/D_xs*V_x;
+			//Viss = D_xs/D_s*Veff_par;
+			Viss = D_xs/D_s*(V_p_par + c_vel);
+			pos = -D_xs/D_s*p_par-D_x/D_s*s_par - shift;
+			
+			for (int i_x = 0; i_x < N_x; i_x++){
+				Phase = phi_x[i_x] + f*pow(im_x[i_x]+pos,2);
+				REAL(i_t,i_nu) += mu_x[i_x]*cos(Phase);
+				IMAG(i_t,i_nu) += mu_x[i_x]*sin(Phase);
+			}
+			REAL(i_t,i_nu) += mu_CPx;
+			shift += Viss*dt;
+		}
+	}
+	
+	#undef  REAL
+	#undef  IMAG
+}
+
+extern "C"
+void  SumStatPoints_1scr_PS_CP_2D (double *E_real, double *E_im, int N_t, int N_nu, double *t, double *nu, int N_x, double *im_x_ra, double *im_x_dec, double D_x, double D_s, double V_x_ra, double V_x_dec, 
+	double *V_p_ra, double *V_p_dec, double *p_ra, double *p_dec, double V_s_ra, double V_s_dec, double s_ra, double s_dec, double *mu_x, double *phi_x, double mu_CPx)
+{
+	#define  REAL(i_t,i_nu)  E_real[(i_t)*N_nu + (i_nu)]
+	#define  IMAG(i_t,i_nu)  E_im[(i_t)*N_nu + (i_nu)]
+	
+	#pragma omp parallel for
+	for (int i_nu = 0; i_nu < N_nu; i_nu++){
+		double Phase;
+		double vc = 299792458.;
+		double D_xs = D_s-D_x;
+		
+		//double Deff = D_x*D_s/D_xs;
+		//double Veff_par;
+		double Viss_ra;
+        double Viss_dec;
+		
+		//double f_delay = M_PI/vc*Deff/D_x/D_x;
+		//double f_doppler = -2.*M_PI/vc*( (p_par + s_par/D_xs*D_x + Veff_par*t[i_t])/D_x );
+        
+        double f = M_PI/vc*(1./D_x+1./D_s)*nu[i_nu];
+		double c_vel_ra = D_x/D_xs*V_s_ra - D_s/D_xs*V_x_ra;
+        double c_vel_dec = D_x/D_xs*V_s_dec - D_s/D_xs*V_x_dec;
+		
+        double shift_ra = 0.;
+        double shift_dec = 0.;
+		double dt = t[1]-t[0];
+		double pos_ra;
+        double pos_dec;
+		
+		for (int i_t = 0; i_t < N_t; i_t++){
+			//Veff_par = V_p_par + D_x/D_xs*V_s_par - D_s/D_xs*V_x;
+			//Viss = D_xs/D_s*Veff_par;
+			Viss_ra = D_xs/D_s*(V_p_ra[i_t] + c_vel_ra);
+            Viss_dec = D_xs/D_s*(V_p_dec[i_t] + c_vel_dec);
+			pos_ra = -D_xs/D_s*p_ra[i_t]-D_x/D_s*s_ra - shift_ra;
+            pos_dec = -D_xs/D_s*p_dec[i_t]-D_x/D_s*s_dec - shift_dec;
+			
+			for (int i_x = 0; i_x < N_x; i_x++){
+				Phase = phi_x[i_x] + f*( pow(im_x_ra[i_x]+pos_ra,2) + pow(im_x_dec[i_x]+pos_dec,2) );
+				REAL(i_t,i_nu) += mu_x[i_x]*cos(Phase);
+				IMAG(i_t,i_nu) += mu_x[i_x]*sin(Phase);
+			}
+			REAL(i_t,i_nu) += mu_CPx;
+			shift_ra += Viss_ra*dt;
+            shift_dec += Viss_dec*dt;
 		}
 	}
 	
@@ -494,4 +648,75 @@ void  SumStatPoints_at_2ndSCR (double *E_real, double *E_im, int N_t, int N_nu, 
 	
 	#undef  REAL
 	#undef  IMAG
+}
+
+extern "C"
+void  IntScreenSimple (int N_th, double t, double nu, double V, double thF, double *th, double *DM, double *E)
+{
+    double dth = th[1] - th[0];
+    // th /= sqrt(2.)*thF;
+    // V /= sqrt(2.)*thF;
+    double phase;
+    double argument;
+    double fact = 1./(2.*pow(thF,2));
+    
+    #pragma omp parallel for
+    for (int i_th = 0; i_th < N_th; i_th++){
+        argument = th[i_th] - V*t;
+        phase = DM[i_th]/nu + fact*argument*argument;
+        E[0] += cos(phase);
+        E[1] += sin(phase);
+    }
+    E[0] *= dth;
+    E[1] *= dth;
+}
+     
+extern "C"
+void  DMI_stripe (int N_t, int N_th, double v_nu, double *thetas, double *phi_disp, double *phi_delay, double *phi_Doppler, double *E_real, double *E_im)
+{
+	#pragma omp parallel for
+	for (int i_t = 0; i_t < N_t; i_t++){
+		double Phase;
+		double phi_fD = phi_Doppler[i_t]*v_nu;
+		for (int i_th = 0; i_th < N_th; i_th++){
+			Phase = phi_disp[i_th]/v_nu + phi_delay[i_th]*v_nu + phi_fD*thetas[i_th];
+			E_real[i_t] += cos(Phase);
+			E_im[i_t] += sin(Phase);
+		}
+	}
+}	 
+
+extern "C"
+void  DMI_time (int N_t, int N_th, double v_nu, double *staus, double *fDMs, double *staus_center, double *staus_mod, double *amps_real, double *amps_im, double *E_real, double *E_im)
+{
+	#pragma omp parallel for
+	for (int i_t = 0; i_t < N_t; i_t++){
+		double Phase;
+        double Dstau;
+        double dstau = staus[1]-staus[0];
+        int i_amp;
+		for (int i_th = 0; i_th < N_th; i_th++){
+            Dstau = staus[i_th]-staus_center[i_t];
+			Phase = fDMs[i_th]/v_nu + v_nu*Dstau*Dstau;
+            i_amp = (int) ( (staus[i_th]+staus_mod[i_t]-staus[0])/dstau + 0.5 );
+			E_real[i_t] += amps_real[i_amp]*cos(Phase)-amps_im[i_amp]*sin(Phase);
+			E_im[i_t] += amps_im[i_amp]*cos(Phase)+amps_real[i_amp]*sin(Phase);
+		}
+	}
+}
+
+extern "C"
+void  DMI_freq (int N_nu, int N_th, double stau_center, double* nus, double *staus, double *fDMs, double *amps_real, double *amps_im, double *E_real, double *E_im)
+{
+	#pragma omp parallel for
+	for (int i_nu = 0; i_nu < N_nu; i_nu++){
+		double Phase;
+        double Dstau;
+		for (int i_th = 0; i_th < N_th; i_th++){
+            Dstau = staus[i_th]-stau_center;
+			Phase = fDMs[i_th]/nus[i_nu] + nus[i_nu]*Dstau*Dstau;
+			E_real[i_nu] += amps_real[i_th]*cos(Phase)-amps_im[i_th]*sin(Phase);
+			E_im[i_nu] += amps_im[i_th]*cos(Phase)+amps_real[i_th]*sin(Phase);
+		}
+	}
 }
